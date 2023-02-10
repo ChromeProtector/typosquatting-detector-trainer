821e659d2ae53d388f336199b7a842fdd4722222
Marcin <xormus@gmail.com>
2023-02-10 23:29:56 +0100

    Add files via upload

---
+++ GenTraningData.ps1
@@ -0,0 +1,43 @@
+﻿param (
+    $protectedDomains,
+    $otherDomains,
+    $typogeneratorPath,
+    $outFile
+)
+
+$lines = [System.Collections.ArrayList]@()
+
+Push-Location
+
+Set-Location $typogeneratorPath
+
+foreach ($otherDomain in $otherDomains)
+{ 
+    $null = $lines.Add($otherDomain + ",0")
+}
+
+foreach ($protectedDomain in $protectedDomains)
+{ 
+     $cmd = 'go run .\cmd\typogen\main.go -s "$protectedDomain"'
+     $out = Invoke-Expression -Command "$cmd"
+    
+     $null = $lines.Add($protectedDomain + ",0")
+
+     $i = 0
+     foreach ($line in $out)
+     {     
+        if ($i -ne 0)
+        {        
+            $parts = $line.Split(",")
+            $null = $lines.Add($parts[3].Trim("""") + ",1")
+        }
+        $i += 1
+     }
+}
+
+$lines | Out-File -FilePath $outFile
+
+Pop-Location
+
+Write-Host $lines.Count
+
